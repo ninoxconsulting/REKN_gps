2124ed83c7d472c1df4f7b5ec11257173ad589db
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2024-07-20 21:01:02 -0700

    update country table

--- 10_Countyr_month_summary.R
+++ 10_Countyr_month_summary.R
@@ -18,10 +18,8 @@ final_dat <- file.path("../../02_data/REKN_gps/output_final")
 out.plots <- file.path("../../02_data/REKN_gps/output_final/figures")
 
 # read in ref data 
-ref <- read_csv(file.path(final_dat, "reference_data_edited.csv"))
-ref_due <- ref %>% select(proj, tag.id, tag.model, study.site) 
-
-#dur <- read_csv( file.path(out.plots, "duration_per_tag_353.csv"))
+#ref <- read_csv(file.path(final_dat, "reference_data_edited.csv"))
+#ref_due <- ref %>% select(proj, tag.id, tag.model, study.site) 
 
 # read in the sub_population list 
 
@@ -31,62 +29,86 @@ pop_id <- pop %>%
          "north", "breeding" , "south","wintering" ,  
          "type", "usable"  ) |> 
   filter(usable == 'y') %>% 
-  filter(subspecies == "rufa") %>%
-  left_join(ref_due)
+  filter(subspecies == "rufa") #%>%
+  #left_join(ref_due)
 
 rufa_ids <- pop_id$tag.id
 
 ## read in compiled data with movements and limit to rufa 
 
-df_all <- st_read(file.path(final_dat,"rekn_moveclass_20240716.gpkg" )) %>% 
-  filter(tag.id %in% rufa_ids ) %>% 
-  filter(movement_final != "uncertain_location")
-
-
-
-df_all <- df_all %>% select(proj, tag.id, date_time, year, month, day, movement_final) %>% 
-  left_join(pop_id)
-  
   
 ###############################################################
-# 
-
-# check the USA 
-
-#### 
-## Estimate the stopover locations per state and month 
-
-install.packages("USA.state.boundaries")
-library(USA.state.boundaries)
-library(ggplot2)
 
 # load tggplot2# load the map
-
-data(state_boundaries_wgs84)
-
-us <- state_boundaries_wgs84 %>% select(NAME,  STATE_ABBR, TYPE)
-st_write(us, file.path(out.plots, "us_states.gpkg"))
+# 
+# data(state_boundaries_wgs84)
+# 
+# us <- state_boundaries_wgs84 %>% select(NAME,  STATE_ABBR, TYPE)
+# st_write(us, file.path(out.plots, "us_states.gpkg"))
 
 # might need to read in the "key" with the assigned subspecies types......
 
 # intersect with country 
 
-world <- ne_countries(scale = "medium", returnclass = "sf") %>% select (admin)
-st_write(world, file.path(out.plots, "world.gpkg"))
+#world <- ne_countries(scale = "medium", returnclass = "sf") %>% select (admin)
+#st_write(world, file.path(out.plots, "world.gpkg"))
 
 
 
-#################################################
+int <- st_read(file.path(final_dat, "rekn_moveclass_20240716_usable_world_ca_us_int.gpkg"))
+ 
+## add the subpop to the table 
+  
+intt <- int |> 
+  select(-"admin", - "NAME", -"STATE_ABBR", -"PRNAME", - "PREABBR" ) |> 
+  left_join(pop_id)%>% 
+  select(-tag.model, -usable, -north, -breeding, -south, -wintering, -tag.id.order, -proj) |> 
+  mutate(weekno = week(date_time)) 
+  
+# 
+# ## Summary by Week of year 
+# cs <- intt |> 
+#   st_drop_geometry() |> 
+#   select(-date_time, -year, -month, -subspecies, -study.site, -type, -movement_final) |> 
+#   filter(!is.na(country)) |> 
+#   distinct()
+# 
+# cs_summ <- cs|> 
+#   group_by(country, state, subpop, weekno) |>
+#   count()
+#   
 
-### read in intersection ....
+## Summary by Month 
+
+ms <- intt |> 
+  st_drop_geometry() |> 
+  select(-date_time, -year, -weekno, -subspecies, -type, -movement_final) |> 
+  filter(!is.na(country)) |> 
+  distinct()
+
+ms_summ <- ms|> 
+  group_by(country, state, subpop, month) |>
+  count()
 
 
 
-rk <- st_intersection(df_all, world)
 
-# intersect with US states 
-rk <- st_intersection(df_all, us)
 
+
+
+
+
+co_sum <- int |> 
+  st_drop_geometry()
+  group_by(year, month, country, state, tag.id) |> 
+  count()
+
+
+
+
+%>% 
+  filter(tag.id %in% rufa_ids ) %>% 
+  filter(movement_final != "uncertain_location")
 
 # plotting with ggplot2
 ggplot(state_boundaries_wgs84) + geom_sf()
